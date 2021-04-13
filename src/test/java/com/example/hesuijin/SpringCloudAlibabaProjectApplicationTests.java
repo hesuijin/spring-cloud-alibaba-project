95ebf353f5206c5ffe5b3b9f69d0d3131a9c88ca
HSJ <hesuijin@qq.com>
2021-04-13 21:27:20 +0800

    新增spring cloud alibaba project

---
+++ src/test/java/com/example/hesuijin/SpringCloudAlibabaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hesuijin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
